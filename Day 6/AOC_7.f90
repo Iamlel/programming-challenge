b32352a534a80a3285d5fc47e88f20d2ea2d5015
Iamlel <88410471+Iamlel@users.noreply.github.com>
2021-12-16 19:19:41 -0800

    Update AOC_7.f90

--- Day 6/AOC_7.f90
+++ Day 6/AOC_7.f90
@@ -1,20 +1,20 @@
-module partone
+module adventOfCode
 	!----------------------------------
 	! Using fortran-utils: https://github.com/certik/fortran-utils
 	
 	use sorting, only: sort
-	use utils, only: loadtxt
+	use myutils, only: loadtxt ! My version of fortran utils. Included in this github
 	use types, only: dp
 	!----------------------------------
 	implicit none
 	
-	integer :: total, num, med, i, j
+	integer :: total, num, num2, i, j
 	integer, allocatable :: list(:)
 	real(dp), allocatable :: d(:, :)
 
 	contains 
 	
-	subroutine startup
+	subroutine partone
 		call loadtxt("puzzle.txt", d)
 		num = size(d)
 		allocate(list(num))
@@ -23,10 +23,26 @@ module partone
 		
 		call sort(list)
 		
-		med = list(floor((num / 2 + 0.5)))
+		num2 = list(floor((num / 2 + 0.5)))
 		total = 0
 		do i = 1, num
-			total = total + abs(list(i) - med)
+			total = total + abs(list(i) - num2)
+		end do
+		
+		print *, total
+	end subroutine
+
+	subroutine parttwo
+		call loadtxt("puzzle.txt", d)
+		num = size(d)
+		allocate(list(num))
+		
+		list(1:num) = int(d(1, 1:num))
+		
+		num2 = ceiling((real(sum(list) / num)))
+		do i = 1, num
+			j = abs(list(i) - num2)
+			total = total + ((j ** 2 + j) / 2)
 		end do
 		
 		print *, total
